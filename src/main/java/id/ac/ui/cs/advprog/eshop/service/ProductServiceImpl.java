ffd4539efff5311db90929f76abe74b37d0cce98
neaurellia <catherineaurellia@gmail.com>
2025-02-10 14:58:32 +0700

    Fixed typo, line 28 ProductServiceImpl.java in service package

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -25,7 +25,7 @@ public class ProductServiceImpl implements ProductService {
     public List<Product> findAll() {
         Iterator<Product> productIterator = productRepository.findAll();
         List<Product> allProduct = new ArrayList<>();
-        productIterator.forEachRemaining(products::add);
+        productIterator.forEachRemaining(allProduct::add);
         return allProduct;
     }
 }
