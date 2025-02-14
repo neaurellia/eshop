3e557d9bc90fd3eaa05b6c7b8163896a3098d8e7
neaurellia <catherineaurellia@gmail.com>
2025-02-14 13:38:57 +0700

    Added positive and negative unit test for create, edit and delete

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -2,15 +2,27 @@ package id.ac.ui.cs.advprog.eshop.model;
 
 import lombok.Getter;
 import lombok.Setter;
+
 import java.util.UUID;
 
-@Getter @Setter
+@Getter
+@Setter
 public class Product {
     private String productId;
     private String productName;
+    private String description;
+    private double price;
     private int productQuantity;
 
     public Product() {
         this.productId = UUID.randomUUID().toString(); // Generate unique ID
+    }
+
+    public Product(String productName, String description, double price, int productQuantity) {
+        this.productId = UUID.randomUUID().toString(); // Generate unique ID
+        this.productName = productName;
+        this.description = description;
+        this.price = price;
+        this.productQuantity = productQuantity;
     }
 }
