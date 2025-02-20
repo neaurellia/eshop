2b46c437193cffe52657aaeddf2a919e6f36a1e5
neaurellia <catherineaurellia@gmail.com>
2025-02-20 16:19:08 +0700

    Created sonarcloud.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,44 @@
-val seleniumJavaVersion = "4.14.1"
-val seleniumJupiterVersion = "5.0.1"
-val webdrivermanagerVersion = "5.6.3"
-val junitJupiterVersion = "5.9.1"
-
 plugins {
     java
     jacoco
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "6.0.1.5171"
 }
-group = "id.ui.ac.cs.advprog"
+
+group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
+
+sonar {
+    properties {
+        property("sonar.projectKey", "neaurellia_eshop")
+        property("sonar.organization", "neaurellia")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
     }
 }
+
 configurations {
     compileOnly {
         extendsFrom(configurations.annotationProcessor.get())
     }
 }
+
 repositories {
     mavenCentral()
 }
+
+val seleniumJavaVersion = "4.14.1"
+val seleniumJupiterVersion = "5.0.1"
+val webdrivermanagerVersion = "5.6.3"
+val junitJupiterVersion = "5.9.1"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -32,11 +47,13 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
     testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
     testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
     testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
 }
+
 tasks.register<Test>("unitTest") {
     description = "Runs unit tests."
     group = "verification"
@@ -54,18 +71,22 @@ tasks.register<Test>("functionalTest") {
         includeTestsMatching("*FunctionalTest")
     }
 }
+
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
 
-tasks.named<Test>("test") {
-    filter {
+tasks.test{
+    filter{
         excludeTestsMatching("*FunctionalTest")
     }
-    finalizedBy("jacocoTestReport") // Ensure JaCoCo always runs
+
+    finalizedBy(tasks.jacocoTestReport)
 }
 
-
-tasks.named<JacocoReport>("jacocoTestReport") {
+tasks.jacocoTestReport{
     dependsOn(tasks.test)
+    reports {
+        xml.required = true
+    }
 }
