0bd71ddc7282e42de566c608654fb41329e9b976
neaurellia <catherineaurellia@gmail.com>
2025-02-28 19:17:05 +0700

    Implemented SOLID
    # Conflicts:
    #	src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -3,13 +3,13 @@ package id.ac.ui.cs.advprog.eshop.service;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
-
-
 public interface ProductService {
     Product create(Product product);
     List<Product> findAll();
     Product findById(String productId);
     void update(Product product);
-    void delete(String productId);
+    void delete(String id);
 }
+
+
 
