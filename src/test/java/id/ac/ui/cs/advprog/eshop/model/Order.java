093b9d9b4d24338f76c67ab3f66e6c71385c08cf
neaurellia <catherineaurellia@gmail.com>
2025-03-07 03:10:44 +0700

    [GREEN] Implement Order model.

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -13,12 +13,27 @@ public class Order {
     List<Product> products;
     Long orderTime;
     String author;
-    @Setter
     String status;
 
     public Order(String id, List<Product> products, Long orderTime, String author) {
+        this.id = id;
+        this.orderTime = orderTime;
+        this.author = author;
+        this.status = "WAITING_PAYMENT";
+
+        if (products.isEmpty()) {
+            throw new IllegalArgumentException();
+        } else {
+            this.products = products;
+        }
     }
 
-    public Order(String id, List<Product> products, Long orderTime, String author, String status) {
+    public void setStatus(String id, List<Product> products, Long orderTime, String author, String status) {
+        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
+        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
     }
 }
