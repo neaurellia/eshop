03749ed51e4ad2a0efabed0a51f01830964ba67b
neaurellia <catherineaurellia@gmail.com>
2025-03-07 13:28:41 +0700

    [RED] Add OrderServiceImpl skeleton.

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImpl.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class OrderServiceImpl implements OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Override
+    public Order createOrder(Order order) {retrun null;}
+
+    @Override
+    public Order updateStatus(String orderId, String status) {return null;}
+
+    @Override
+    public List<Order> findAllByAuthor(String author) {return null;}
+
+    @Override
+    public Order findById(String orderId) {return null;}
+
+}
