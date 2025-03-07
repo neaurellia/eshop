6e1b1847a2aa7da5324839a95e1db6661ac6151e
neaurellia <catherineaurellia@gmail.com>
2025-03-07 12:23:50 +0700

    [GREEN] Implement OrderRepository class.

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
@@ -3,10 +3,39 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class OrderRepository {
     private List<Order> orderData = new ArrayList<>();
-    public Order save (Order order) {return null;}
-    public Order findById(String id) {return null;}
-    public List<Order> findAllByAuthor(String Author) {return null;}
+    public Order save (Order order) {
+        int i = 0;
+        for (Order savedOrder : orderData) {
+            if (savedOrder.getId().equals(order.getId())) {
+                orderData.remove(i);
+                orderData.add(i, order);
+                return order;
+            }
+            i += 1;
+        }
+
+        orderData.add(order);
+        return order;
+    }
+    public Order findById(String id) {
+        for (Order savedOrder : orderData) {
+            if (savedOrder.getId().equals(id)) {
+                return savedOrder;
+            }
+        }
+        return null;
+    }
+    public List<Order> findAllByAuthor(String Author) {
+        List<Order> orders = new ArrayList<>();
+        for (Order savedOrder : orderData) {
+            if (savedOrder.getAuthor().equals(author)) {
+                result.add(savedOrder);
+            }
+        }
+        return result;
+    }
 }
