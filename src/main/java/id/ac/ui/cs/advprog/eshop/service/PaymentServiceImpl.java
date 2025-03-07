9992eb53bfe4576efbc4bdceb9acfba14cc833ca
neaurellia <catherineaurellia@gmail.com>
2025-03-07 22:27:35 +0700

    [GREEN] Implement PaymentServiceImpl class.

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -2,9 +2,11 @@ package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
@@ -16,21 +18,36 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public Payment createPayment(Payment payment) {
-        return null;
+        if (paymentRepository.findById(payment.getId()) != null) {
+            throw new IllegalArgumentException("Payment with this ID already exists.");
+        }
+        return paymentRepository.save(payment);
     }
 
     @Override
     public Payment updateStatus(String paymentId, String status) {
-        return null;
+        Payment payment = paymentRepository.findById(paymentId);
+        if (payment == null) {
+            throw new NoSuchElementException("Payment not found.");
+        }
+        if (!PaymentStatus.isValid(status)) {
+            throw new IllegalArgumentException("Invalid payment status.");
+        }
+        payment.setStatus(status);
+        return paymentRepository.save(payment);
     }
 
     @Override
     public Payment findById(String paymentId) {
-        return null;
+        Payment payment = paymentRepository.findById(paymentId);
+        if (payment == null) {
+            throw new NoSuchElementException("Payment not found.");
+        }
+        return payment;
     }
 
     @Override
     public List<Payment> findAllByOrderId(String orderId) {
-        return null;
+        return paymentRepository.findAllByOrderId(orderId);
     }
 }
